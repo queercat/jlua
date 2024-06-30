35dc63316e38eab8bc0d363addff327ce91451cd
May Tusek <tusek.may@gmail.com>
2024-06-30 00:16:54 -0700

    Updates lexer to add EOF token.

--- src/lexer.ts
+++ src/lexer.ts
@@ -2,6 +2,7 @@ export type Token = {
     value: string
     start: number
     end: number
+    eof: boolean
 }
 
 const composeRegex = (...regex: RegExp[]) => new RegExp(regex.map(r => r.source).join("|"))
@@ -14,9 +15,9 @@ const validToken = composeRegex(validAscii, validSymbol)
 export const lex = (source: string, offset: number = 0, matches: Token[] = []): Token[] => {
     const symbol = validToken[Symbol.match](source.substring(offset))
 
-    if (!symbol) return matches
+    if (!symbol) return [...matches, {value: "EOF", start: offset, end: offset, eof: true}]
 
-    matches.push({start: symbol.index! + offset, end: symbol.index! + offset + symbol[0].length, value: symbol[0]})
+    matches.push({start: symbol.index! + offset, end: symbol.index! + offset + symbol[0].length, value: symbol[0], eof: false})
 
     return lex(source, offset + (symbol.index ?? 0) + symbol[0].length, matches)
 }
