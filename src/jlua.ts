8ceca5dd8861fdad2d41786463743e46132a104e
May Tusek <tusek.may@gmail.com>
2024-07-04 14:41:44 -0700

    Instantiates parser class.

--- src/jlua.ts
+++ src/jlua.ts
@@ -1,7 +1,9 @@
 import { lex } from "./lexer";
+import { Parser } from "./parser";
 
 const source: string = `return 1, true, 2, false, 3, 4`;
+const parser = new Parser();
 
-const ast = parse(lex(source));
+const ast = parser.parse(lex(source));
 
 console.log(ast);
