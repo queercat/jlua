7718fe435b88d0cd0a9d33488eb54169007ef028
May Tusek <tusek.may@gmail.com>
2024-07-04 21:26:57 -0700

    Begins work on parser.

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,19 @@
 import { Token } from "./lexer";
+import { Block, Chunk, ReturnStatement, Statement } from "./types";
 
 export class Parser {
   parse = (tokens: Token[]) => {};
+
+  parseBlock = (): Block => {
+    const statements: Statement[] = [];
+    let returnStatement: ReturnStatement | undefined;
+
+    return new Block(statements, returnStatement);
+  };
+
+  idx: number;
+
+  constructor() {
+    this.idx = 0;
+  }
 }
